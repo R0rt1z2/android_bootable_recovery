404ce06d91c6b39fd0e81d6a399b669b2ce330c3
Joel Stein <js@funkturm.net>
2019-02-14 06:57:01 +0100

    Make amonet selectable using TW_AMONET and update microloader

--- Android.mk
+++ Android.mk
@@ -236,6 +236,9 @@ endif
 ifeq ($(TW_NO_REBOOT_BOOTLOADER), true)
     LOCAL_CFLAGS += -DTW_NO_REBOOT_BOOTLOADER
 endif
+ifeq ($(TW_AMONET), true)
+    LOCAL_CFLAGS += -DTW_AMONET
+endif
 ifeq ($(TW_NO_REBOOT_RECOVERY), true)
     LOCAL_CFLAGS += -DTW_NO_REBOOT_RECOVERY
 endif
